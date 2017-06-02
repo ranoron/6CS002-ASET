9f2209f895783829924ee6f0c216691bba8d2e4a
ranoron <ranorona@gmail.com>
2017-06-02 07:06:17 +0530

    all the clases addd to repository

---
+++ randle015/IOSpecialist.java
@@ -0,0 +1,14 @@
+package randle015;
+
+/**
+ * @author Kevan Buckley, maintained by James Randle
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
---
+++ randle015/Slough.java
@@ -0,0 +1,14 @@
+package randle015;
+public class Slough {
+  static transient String[] tiger = {
+    "If one is only to talk from first-hand experience, conversation would be a very poor business.","C.S. Lewis","",
+    "You only lie to two people in your life: your girlfriend and the police.","Jack Nicholson","",
+    "The most incomprehensible thing about the world is that it is at all comprehensible.","Albert Einstein","",
+    "Think of all the beauty that's still left in and around you, and be happy.","Anne Frank","",
+    "Conscience is the inner voice that warns us somebody may be looking.","Henry Mencken","",
+    "Pride gets no pleasure out of having something, only out of having more of it than the next man.","C.S. Lewis","",
+    "Friends, if we be honest with ourselves, we shall be honest with each other.","George MacDonald","",
+    "Time passes, and little by little everything that we have spoken in falsehood becomes true.","Marcel Proust","",
+    "Speaking the Truth in times of universal deceit is a revolutionary act.","George Orwell","",
+    "Though I am not naturally honest, I am sometimes by chance.","William Shakespeare",""};
+}
